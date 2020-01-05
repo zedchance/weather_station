8ff3a9502aa64c5f31514213efba5b71b6e9554f
Zed Chance <zchance@sierracollege.edu>
2020-01-04 21:38:19 -0800

    server.c waits for more input

--- server/server.c
+++ server/server.c
@@ -6,26 +6,43 @@
  */
 
 void get_conditions();
+char get_choice();
 
 int main(int argc, char *argv[])
 {
     // No arguments
-    if (argc < 2)
-    {
-        printf("No arguments.\n");
-        exit(1);
-    }
+    // if (argc < 2)
+    // {
+    //     printf("No arguments.\n");
+    //     exit(1);
+    // }
 
-    // Switch commands
-    switch(argv[1][0])
+    while(1)
     {
-        case 'G':
-            get_conditions();
-            break;
-        default:
-            fprintf(stderr, "ERR: I don't know that command.\n");
-            break;
+        // Get choice
+        char choice = get_choice();
+
+        // Switch commands
+        switch(choice)
+        {
+            case 'G':
+                get_conditions();
+                break;
+            case 'Q':
+                exit(0);
+                break;
+            default:
+                fprintf(stderr, "ERR: I don't know that command.\n");
+                break;
+        }
     }
+}
+
+char get_choice()
+{
+    char buf[100];
+    fgets(buf, 100, stdin);
+    return buf[0];
 }
 
 void get_conditions()
