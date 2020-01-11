1ea0089e158b53b4317417876d3a07c8d7db1e29
Zed Chance <zchance@sierracollege.edu>
2020-01-10 21:34:02 -0800

    Decided to finally go with perl for the server

---
+++ server/server.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Socket;
+
+# Port number 12345
+my $port = 12345;
+my $proto = getprotobyname('tcp');
+my $server = "raspberrypi";
+
+# Create a socket
+socket(SOCKET, PF_INET, SOCK_STREAM, $proto)
+	or die "Can't open socket $!\n";
+setsockopt(SOCKET, SOL_SOCKET, SO_REUSEADDR, 1)
+	or die "Can't set socket option to SO_REUSEADDR $!\n";
+
+# Bind to port and listen
+bind(SOCKET, pack_sockaddr_in($port, inet_aton($server)))
+	or die "Can't bind to port $port!\n";
+listen(SOCKET, 5) or die "Listen: $!\n";
+print "SERVER started on port $port.\n";
+
+# Accept incoming connection
+my $client_addr;
+while ($client_addr = accept(NEW_SOCKET, SOCKET))
+{
+	# Pull new weather and respond
+	`python3.7 pull.py`;
+	my $name = gethostbyaddr($client_addr, AF_INET);
+	print NEW_SOCKET `cat current_conditions`;
+	print "Connection received from $name.\n";
+	close NEW_SOCKET;
+}
